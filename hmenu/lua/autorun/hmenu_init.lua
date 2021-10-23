2721275bbe35fa65a01681e8d01638e4a1ecf496
Xenon <hankjwimbleton@ya.ru>
2021-10-23 15:46:15 +0500

    Update hmenu_init.lua
    changes in code style

--- hmenu/lua/autorun/hmenu_init.lua
+++ hmenu/lua/autorun/hmenu_init.lua
@@ -1,73 +1,53 @@
 if SERVER then
+	local folderName = 'hmenu'
+	local filesInFolder = file.Find(folderName .. '*', 'LUA')
 
-	local Folder = "hmenu"
-	local Files = file.Find( Folder .. "*", "LUA" )
-
-	for k, v in pairs( Files ) do
-
-		include( Folder .. "/" .. v )
-
+	for k, v in pairs(filesInFolder) do
+		include(folderName .. '/' .. v)
 	end
 
-	local Folder = "hmenu/sh"
+	local folderName = 'hmenu/sh'
 
-	for _, File in SortedPairs( file.Find( Folder .. "/sh_*.lua", "LUA" ), true ) do
-
-		AddCSLuaFile( Folder .. "/" .. File )
-		include( Folder .. "/" .. File )
-
+	for _, File in SortedPairs(file.Find(folderName .. '/sh_*.lua', 'LUA'), true) do
+		AddCSLuaFile(folderName .. '/' .. File)
+		include(folderName .. '/' .. File)
 	end
 
-	local Folder = "hmenu/sv"
+	local folderName = 'hmenu/sv'
 
-	for _, File in SortedPairs( file.Find( Folder .. "/sv_*.lua", "LUA" ), true ) do
-
-		include( Folder .. "/" .. File )
-
+	for _, File in SortedPairs(file.Find(folderName .. '/sv_*.lua', 'LUA'), true) do
+		include(folderName .. '/' .. File)
 	end
 
-	local Folder = "hmenu/cl"
+	local Folder = 'hmenu/cl'
 
-	for _, File in SortedPairs( file.Find( Folder .. "/cl_*.lua", "LUA" ), true ) do
-
-		AddCSLuaFile( Folder .. "/" .. File )
-
+	for _, File in SortedPairs(file.Find(folderName .. '/cl_*.lua', 'LUA'), true) do
+		AddCSLuaFile(folderName .. '/' .. File)
 	end
 
-	local Folder = "hmenu/vgui"
+	local folderName = 'hmenu/vgui'
 
-	for _, File in SortedPairs( file.Find( Folder .. "/vgui_*.lua", "LUA" ), true ) do
-
-		AddCSLuaFile( Folder .. "/" .. File )
-
+	for _, File in SortedPairs(file.Find(folderName .. '/vgui_*.lua', 'LUA'), true) do
+		AddCSLuaFile(folderName .. '/' .. File)
 	end
-
 end
 
 if CLIENT then
+	local folderName = 'hmenu/sh'
 
-	local Folder = "hmenu/sh"
-
-	for _, File in SortedPairs( file.Find( Folder .. "/sh_*.lua", "LUA" ), true ) do
-
-		include( Folder .. "/" .. File )
-
+	for _, File in SortedPairs(file.Find(folderName .. '/sh_*.lua', 'LUA'), true) do
+		include(folderName .. '/' .. File)
 	end
 
-	local Folder = "hmenu/cl"
+	local folderName = 'hmenu/cl'
 
-	for _, File in SortedPairs( file.Find( Folder .. "/cl_*.lua", "LUA" ), true ) do
-
-		include( Folder .. "/" .. File )
-
+	for _, File in SortedPairs(file.Find(folderName .. '/cl_*.lua', 'LUA'), true) do
+		include(folderName .. '/' .. File)
 	end
 
-	local Folder = "hmenu/vgui"
+	local folderName = 'hmenu/vgui'
 
-	for _, File in SortedPairs( file.Find( Folder .. "/vgui_*.lua", "LUA" ), true ) do
-
-		include( Folder .. "/" .. File )
-
+	for _, File in SortedPairs(file.Find(folderName .. '/vgui_*.lua', 'LUA'), true) do
+		include(folderName .. '/' .. File)
 	end
-
 end
